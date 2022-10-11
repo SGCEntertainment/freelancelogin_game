5e32c8cf1eacb2dc6becdc0df31563e3d84e3559
Sladkiy Eduard <smallgamescompany@gmail.com>
2022-10-11 20:41:13 +0300

    update

--- Assets/OpenCV+Unity/Demo/Face_Detector/FaceDetectorScene.cs
+++ Assets/OpenCV+Unity/Demo/Face_Detector/FaceDetectorScene.cs
@@ -68,7 +68,7 @@
 			// mark detected objects
 			processor.MarkDetected();
 
-			outText.text = $"headY:{processor.Faces[0].Region.Y} & lineY:{line.localPosition.y}";
+			outText.text = $"Head Center:{processor.Faces[0].Region.Center} & line Y:{line.localPosition.y}";
 
 			// processor.Image now holds data we'd like to visualize
 			output = Unity.MatToTexture(processor.Image, output);   // if output is valid texture it's buffer will be re-used, otherwise it will be re-created
